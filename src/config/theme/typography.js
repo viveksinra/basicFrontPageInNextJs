7d7aa8832c4650094a88b63d74ac136546f5cbb3
viveksinra <vivekkrvks@gmail.com>
2024-01-26 22:18:04 +0530

    ts to js

--- src/config/theme/typography.js
+++ src/config/theme/typography.js
@@ -1,12 +1,10 @@
-import { TypographyOptions } from '@mui/material/styles/createTypography'
-
 export const fontFamily = [
-  '"Cabin",-apple-system, BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
-].join(',')
+  '"Cabin",-apple-system, BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"'
+].join(",")
 
 const headingLineHeight = 1.4
 
-const typography: TypographyOptions = {
+const typography = {
   fontFamily,
   fontWeightLight: 400,
   fontWeightRegular: 400,
@@ -15,45 +13,45 @@ const typography: TypographyOptions = {
   h1: {
     fontSize: 34,
     lineHeight: headingLineHeight,
-    fontWeight: 700,
+    fontWeight: 700
   },
   h2: {
     fontSize: 28,
     lineHeight: headingLineHeight,
-    fontWeight: 700,
+    fontWeight: 700
   },
   h3: {
     fontSize: 24,
     lineHeight: headingLineHeight,
-    fontWeight: 700,
+    fontWeight: 700
   },
   h4: {
     fontSize: 22,
     lineHeight: headingLineHeight,
-    fontWeight: 700,
+    fontWeight: 700
   },
   h5: {
     fontSize: 17,
     lineHeight: headingLineHeight,
-    fontWeight: 600,
+    fontWeight: 600
   },
   h6: {
     fontSize: 15,
     lineHeight: headingLineHeight,
-    fontWeight: 600,
+    fontWeight: 600
   },
   body1: {
-    fontSize: '1rem',
+    fontSize: "1rem"
   },
   body2: {
-    fontSize: '0.9rem',
+    fontSize: "0.9rem"
   },
   subtitle1: {
-    fontSize: '0.85rem',
+    fontSize: "0.85rem"
   },
   subtitle2: {
-    fontSize: '0.8rem',
-  },
+    fontSize: "0.8rem"
+  }
 }
 
 export default typography
